c41c66163a50717eefeaeb163f1ad74ad03e1f47
Ivan <mr.sitnikov.ru@yandex.ru>
2023-03-03 15:01:25 +0300

    Activity lifecycle

--- app/src/main/java/com/example/p1/MainActivity.java
+++ app/src/main/java/com/example/p1/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.p1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+    protected void onStart() {
+        super.onStart();
+    }
+    protected void onPause() {
+        super.onPause();
+
+    }
+    protected void onResume() {
+        super.onResume();
+
+    }
+    protected void onStop() {
+        super.onStop();
+
+    }
+    protected void onDestroy() {
+        super.onDestroy();
+
     }
 }
